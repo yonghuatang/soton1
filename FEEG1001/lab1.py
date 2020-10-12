14e6e83a79b55cba4359c443d61f8d3e481ee7d6
Yong Hua Tang <70836155+yonghuatang@users.noreply.github.com>
2020-10-12 10:16:46 +0800

    lab1

---
+++ FEEG1001/lab1.py
@@ -0,0 +1,36 @@
+# Lab 1
+# 12 October 2020
+# Created by YongHua
+
+import math
+
+def average(a, b):
+    return (a + b) * 0.5
+
+
+print(average(10, 20))
+print(average(10, 4))
+
+def distance(a, b):
+    if a > b:
+        return a - b
+    else:
+        return b - a
+
+
+print(distance(3, 4))
+print(distance(3, 1))
+
+def geometric_mean(a, b):
+    return math.sqrt(a * b)
+
+
+print(geometric_mean(2, 2))
+print(geometric_mean(2, 8))
+print(geometric_mean(2, 1))
+
+def pyramid_volume(A, h):
+    return (1/3) * A * h
+
+
+print(pyramid_volume(1, 2))
